88c4bee3a69f7ccaa6969f53a7576b3e7db49477
RioHiroko <ramanamahefafabrice@gmail.com>
2024-09-30 08:31:20 +0300

    modife Entité

--- src/Entity/Message.php
+++ src/Entity/Message.php
@@ -1,23 +1,87 @@
 <?php
+
 namespace App\Entity;
 
+use App\Repository\MessageRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
-#[ORM\Entity]
+#[ORM\Entity(repositoryClass: MessageRepository::class)]
 class Message
 {
-    #[ORM\Id, ORM\GeneratedValue, ORM\Column(type: 'integer')]
-    private $id;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
 
     #[ORM\ManyToOne(targetEntity: User::class)]
-    private $sender;
+    #[ORM\JoinColumn(nullable: false)]
+    #[Assert\NotBlank]
+    private ?User $sender = null;
 
     #[ORM\ManyToOne(targetEntity: User::class)]
-    private $receiver;
+    #[ORM\JoinColumn(nullable: false)]
+    #[Assert\NotBlank]
+    private ?User $receiver = null;
 
     #[ORM\Column(type: 'text')]
-    private $content;
+    #[Assert\NotBlank]
+    private ?string $content = null;
 
     #[ORM\Column(type: 'datetime')]
-    private $sentAt;
+    #[Assert\NotNull]
+    private ?\DateTimeInterface $sentAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSender(): ?User
+    {
+        return $this->sender;
+    }
+
+    public function setSender(User $sender): static
+    {
+        $this->sender = $sender;
+
+        return $this;
+    }
+
+    public function getReceiver(): ?User
+    {
+        return $this->receiver;
+    }
+
+    public function setReceiver(User $receiver): static
+    {
+        $this->receiver = $receiver;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getSentAt(): ?\DateTimeInterface
+    {
+        return $this->sentAt;
+    }
+
+    public function setSentAt(\DateTimeInterface $sentAt): static
+    {
+        $this->sentAt = $sentAt;
+
+        return $this;
+    }
 }
