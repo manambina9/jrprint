ac6fb07fac1e4979a1d5987115726d9880502c2d
manambina9 <kellyandell9@gmail.com>
2024-10-01 15:36:07 +0200

    Dashbord Admin

---
+++ src/Controller/Admin/PrestationCrudController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Prestation;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+
+class PrestationCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Prestation::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+        ->setEntityLabelInSingular('Service')
+        ->setEntityLabelInPlural('Services')
+
+        ->setPageTitle("index", "Jr_Print | Administration Prestation")
+
+        ->setPaginatorPageSize(20)
+
+        ->addFormTheme('@FOSCKEditor/Form/ckeditor_widget.html.twig');
+    }
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description')
+                ->setFormType(CKEditorType::class)
+                ->hideOnIndex(),
+        ];
+    }
+    
+}
