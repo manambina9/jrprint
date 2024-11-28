1cbe3c4ea482594cd9a85bf9c0a46ec518d03715
RioHiroko <ramanamahefafabrice@gmail.com>
2024-11-28 09:57:22 +0300

    UserCheckOUT

--- src/Entity/Prestation.php
+++ src/Entity/Prestation.php
@@ -51,6 +51,9 @@ class Prestation
     #[ORM\Column(type: 'datetime', nullable: true)]
     private ?\DateTimeInterface $updatedAt = null;
 
+    #[ORM\Column(type: 'decimal', precision: 10, scale: 2)]
+    private ?float $price = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -179,6 +182,17 @@ class Prestation
     public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
     {
         $this->updatedAt = $updatedAt;
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?float $price): self
+    {
+        $this->price = $price;
         return $this;
     }
 
